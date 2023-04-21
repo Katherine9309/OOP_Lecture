8d59300e56dea355261a18c5ca46737804dac51a
katherin cucunuba <66915247+Katherine9309@users.noreply.github.com>
2023-04-21 01:10:57 -0500

    Hierarchy description

--- Task 2/Program_2.cs
+++ Task 2/Program_2.cs
@@ -8,6 +8,9 @@ namespace Task_2
     {
         static void Main(string[] args)
         {
+            //from live beings there are two groups animals and plants where animals is divided on three groups (carnivore,omnivore,herbivore) 
+            //and the plants is divide on two groups (herbs and scrub) , addicional when you are carnivore animal you have the posibility
+            //to be predator
             Console.WriteLine("Hello Taks 2");
             //Console.WriteLine(/t);
             Carnivore wolf = new Carnivore("wolf"){ispredator=true};
