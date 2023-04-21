24a799418f2c6f58db788acc3414476a9f776fef
katherin cucunuba <66915247+Katherine9309@users.noreply.github.com>
2023-04-20 22:36:02 -0500

    Solution task 5

---
+++ Task 5/Polynomial.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_5
+{
+    class Polynomial
+    {
+        private int x0,x1,x2,x3,x4;
+        public Polynomial(int n0,int n1,int n2,int n3,int n4)
+        {
+            x0 = n0;
+            x1 = n1;
+            x2 = n2;
+            x3 = n3;
+            x4 = n4;
+        }
+      
+        public override string ToString() {
+            return $"{x0} + {x1}x^2 + {x2}x^5 - {x3}x^7";
+        }
+
+
+        public static Polynomial operator +(Polynomial p0, Polynomial p1) {
+          
+            return new Polynomial(p0.x0+p1.x0, p0.x1 + p1.x1, p0.x2 + p1.x2, p0.x3 + p1.x3, p0.x4 + p1.x4);
+        }
+
+        public static Polynomial operator -(Polynomial p0, Polynomial p1)
+        {
+
+            return new Polynomial(p0.x0 - p1.x0, p0.x1 - p1.x1, p0.x2 - p1.x2, p0.x3 - p1.x3, p0.x4 - p1.x4);
+        }
+
+        public static Polynomial operator *(Polynomial p0, Polynomial p1)
+        {
+
+            return new Polynomial(p0.x0 * p1.x0, p0.x1 * p1.x1, p0.x2 * p1.x2, p0.x3 * p1.x3, p0.x4 * p1.x4);
+        }
+
+    }
+}
---
+++ Task 5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Task_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //ToString(Polynomial());
+            // Polynomial().ToString();
+            var number = new Polynomial(-3,4,5,6,7);
+            Console.WriteLine("polynomial 1: "+ number.ToString());
+            var number2 = new Polynomial(-3, 4, 5, 6, 7);
+            Console.WriteLine("polynomial 2: "+ number2.ToString());
+            var number3 = number - number2;
+            Console.WriteLine("addiction: " + number3.ToString());
+            number3 = number - number2;
+            Console.WriteLine("subtraction: "+ number3.ToString());
+            number3 = number * number2;
+            Console.WriteLine("multiplication: " + number3.ToString());
+        }
+    }
+}
