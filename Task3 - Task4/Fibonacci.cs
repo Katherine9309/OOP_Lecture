21f74678c7534dca34f27584c4a74a4ce150dc59
katherin cucunuba <66915247+Katherine9309@users.noreply.github.com>
2023-04-20 20:11:49 -0500

    Solution for the task3 and task4

---
+++ Task3 - Task4/Fibonacci.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3___Task4
+{
+    class Fibonacci
+    {
+        public int limitNumber;
+        public Fibonacci (int number)
+        {
+            limitNumber = number;
+        }
+
+        public void GetSerie()
+        {
+            int a = 0, b = 1;
+            Console.Write("{0} {1} {2}", a, b,b+a);
+            for (int c = 2; c <= limitNumber; c = a + b)
+            {
+
+                Console.Write(" {0}", c);
+                a = b;
+                b = c;
+            }
+        }
+    }
+}
---
+++ Task3 - Task4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Task3___Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Type the numeber: ");
+            string input = Console.ReadLine();
+            int number = Convert.ToInt32(input);
+            Fibonacci fibonacci = new Fibonacci(number);
+            Console.WriteLine("Fibonacci with loop");
+            fibonacci.GetSerie();
+            Console.WriteLine();
+            Console.WriteLine("Fibonacci with recursion");
+            int a = 0, b = 1, c = 2;
+            Console.Write("{0} {1} {2}", a, b, b + a);
+            FibonacciRecursion(number,a,b ,c);
+        }
+        static void FibonacciRecursion(int number,int a,int b, int c) {
+            if ( c <= number) {
+                Console.Write(" {0}", c);
+             
+                a = b;
+                b = c;
+                c = a + b;
+                FibonacciRecursion(number, a, b, c);
+            }
+        }
+    }
+}
